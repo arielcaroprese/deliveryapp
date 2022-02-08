2b7f4d386bc28f4b4ce6495be7d9c85db441577d
Lucia <lucy_17_4@hotmail.com>
2022-02-08 16:01:59 -0300

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.scss';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from './views/Home.js';
+import Product from './views/Product.js';
 
 function App() {
   return (
-    <>
-      <Home/>
-    </>
+    <BrowserRouter>
+
+      <Switch>
+
+        <Route path="/">
+          <Home/>
+        </Route>
+
+        <Route path="/product">
+          <Product/>
+        </Route>
+
+      </Switch>
+
+    </BrowserRouter>
   );
 }
 
